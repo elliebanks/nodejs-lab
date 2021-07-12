c5db1e0fbdad32968ffe06170dc8497d7538c186
Ellie Banks <ellenebanks5@gmail.com>
2021-07-12 12:37:58 -0400

    popular.download.js added

--- popular.downloader.js
+++ popular.downloader.js
@@ -1,6 +1,34 @@
 const path = require('path');
 const fs = require('fs');
-const request = require('request');
+//const request = require('request');
 const rp = require('request-promise');
 
-//Um I have no idea how to do this advanced part.
+const options = {
+    json: true,
+}
+
+rp('https://reddit.com/r/popular.json', options)
+    .then(function (payLoad) {
+        payLoad.data.children.forEach(function (item) {
+            if (path.extname(item.data.url) == ".jpg" || path.extname(item.data.url) == ".png" || path.extname(item.data.url) == ".gif") {
+                const imageRequestOptions = {
+                    url: item.data.url,
+                    encoding: "base64"
+                };
+
+                rp(imageRequestOptions)
+                    .then(function (image) {
+                        fsWriteFile(`./downloads/${item.data.id}$/{path.extname(item.data.url)}`, image, "base64", function (err) {
+                            if (err) throw err;
+                            console.log("saved");
+                        })
+                    })
+
+            }
+        })
+
+    })
+
+    .catch(function (err) {
+        console.log(err);
+    });
